6b3650927d5adedec926d40aa063d32a606fd6b2
Technical-Turbulence-23344 <techtcoders@gmail.com>
2024-03-30 19:47:52 -0500

    ManualMotorDriveTesting.java V1 added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTuner/BehindTheScenes/DriveConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTuner/BehindTheScenes/DriveConfig.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.TeleopTuner.BehindTheScenes;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -9,8 +10,12 @@ import com.qualcomm.robotcore.hardware.Servo;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
-
+@Disabled
 public class DriveConfig {
+
+    //TODO: Update the below string based on your odometry pods
+    // Your options are "none", "two", or "three", depending on your odo pod setup
+    public static final String ODO_TYPE = "none";
 
     //TODO: Change the motor declaration ID's
 
@@ -48,15 +53,32 @@ public class DriveConfig {
     }};
 
 
-    //TODO: Add more motor variables if needed
-    // You MUST name them MOTOR_N, where N is the motor number
 
-    public static final DcMotorEx MOTOR_1 = MOTOR_IDS.get(1);
-    public static final DcMotorEx MOTOR_2 = MOTOR_IDS.get(2);
-    public static final DcMotorEx MOTOR_3 = MOTOR_IDS.get(3);
-    public static final DcMotorEx MOTOR_4 = MOTOR_IDS.get(4);
+    public static final List<DcMotorEx> ENCODER_IDS =new ArrayList<DcMotorEx>(){{
+        add(hardwareMap.get(DcMotorEx.class,""));
+        //TODO: DO NOT CHANGE/DELETE THE ABOVE LINE. IT IS USED FOR INDEX CONVENIENCE
+        //TODO: If you are using encoders for your drivetrain motors or any other motor, declare them here
+        //These 8 lines of code correspond with the MOTOR_NAMES list
+        //Let us say you plugged in the frontRightMotor encoder into the encoder slot corresponding with the                   backRightMotor
+        //As the frontRightMotor is the 2th line in the MOTOR_NAMES list, you will declare the encoder in the 2nd line         of the ENCODER_IDS list
+        //Replace the 2nd line with:
+        //add(MOTOR_4);
+        //you are doing MOTOR_4 because the encoder is plugged into the backRightMotor encoder slot, which is the 4th on the MOTOR_IDS list (and MOTOR_NAMES list)
 
-    //Example: public static final DcMotorEx MOTOR_5 = MOTOR_IDS.get(5);
+        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(1)
+        add(hardwareMap.get(DcMotorEx.class,"")); //The line for the example, Corresponds with MOTOR_NAMES.get(2)
+        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(3)
+        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(4)
+
+        //TODO:Add the below lines if you have more than 4 motors
+        //The size of this list should equal the size of the MOTOR_NAMES list and the MOTOR_IDS list
+
+//        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(5)
+//        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(6)
+//        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(7)
+//        add(hardwareMap.get(DcMotorEx.class,"")); //Corresponds with MOTOR_NAMES.get(8)
+
+    }};
 
     //TODO: If you are using encoders for your drivetrain motors or any other motor, declare them here
     // You MUST name them ENCODER_N, where N corresponds with the motor number above
@@ -72,5 +94,14 @@ public class DriveConfig {
 
     //Example: public static final DcMotorEx ODO_LEFT = MOTOR_IDS.get(6);
     
+
+
+    //TODO: If you have odometry pods, declare the below.
+    //YOU CANNOT CHANGE THE NAME OF THE ODO PODS
+    //The 2/3 names for your odo pods are:
+    //ODO_LEFT, ODO_RIGHT, ODO_SIDE
+
+    //Example: public static final DcMotorEx ODO_LEFT = MOTOR_IDS.get(6);
+
 
 }
