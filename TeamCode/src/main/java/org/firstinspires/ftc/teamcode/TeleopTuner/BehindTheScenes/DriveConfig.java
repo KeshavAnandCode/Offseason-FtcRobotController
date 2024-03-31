044e91fbdfdbbffa41047bdceeec05277ef17edd
Technical-Turbulence-23344 <techtcoders@gmail.com>
2024-03-30 20:54:48 -0500

    ManualMotorDriveTesting.java V1 added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTuner/BehindTheScenes/DriveConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTuner/BehindTheScenes/DriveConfig.java
@@ -34,7 +34,6 @@ public class DriveConfig {
         //back right motor ID
         //TODO: Add any more motors to this list if you have them
         //Example: add(hardwareMap.get(DcMotorEx.class, "servoArm"));
-
     }};
 
     public static final List<String> MOTOR_NAMES = new ArrayList<String>(){{
