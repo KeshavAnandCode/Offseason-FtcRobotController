d3394268eaa35916616bfa4d155e3e403009dca8
KeshavAnandCode <163459625+KeshavAnandCode@users.noreply.github.com>
2024-04-17 16:36:26 +0000

    Deleted driveconfig.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopTuner/BehindTheScenes/DriveConfig.java
+++
@@ -1,8 +0,0 @@
-package org.firstinspires.ftc.teamcode.TeleopTuner.BehindTheScenes;
-
-public class DriveConfig {
-    public static final double WHEEL_DIAMETER = 4.0; // Example value (in inches)
-    public static final double TICKS_PER_REVOLUTION = 1440; // Example value
-
-    // Add any other necessary configuration constants or methods
-}
