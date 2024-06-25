0a28968ba05031746ee4ad208fe28a33a250b3f2
KeshavAnandCode <keshavmathguy@gmail.com>
2024-06-24 21:59:49 -0500

    6/24

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RRTest.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
@@ -9,24 +10,35 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Autonomous.RR.MecanumDrive;
+import org.firstinspires.ftc.teamcode.Teleop.BehindTheScenes.Singletons.Robot;
 
-@Disabled
 @Autonomous
+@Config
 public class RRTest extends LinearOpMode {
+
+    Robot robot;
     @Override
     public void runOpMode() throws InterruptedException {
-        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+
+        robot = new Robot (hardwareMap);
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,Math.toRadians(0)));
+
+        //Trajectories go here
 
         Action trajectoryAction1 = drive.actionBuilder(drive.pose)
                 .setTangent(0)
-                .splineToConstantHeading(new Vector2d(20,5),0)
+                .splineToSplineHeading(new Pose2d(20,20,Math.toRadians(0)),0)
                 .setTangent(0)
-                .splineToSplineHeading(new Pose2d(48,-5,Math.PI /2),0)
+                .splineToSplineHeading(new Pose2d(0,0,0),Math.toRadians(0))
+                .strafeToLinearHeading(new Vector2d(10,10),0.8)
                 .build();
 
         waitForStart();
 
         if (isStopRequested()) return;
+
+        //Autonomous Code goes here
 
         Actions.runBlocking(trajectoryAction1);
     }
